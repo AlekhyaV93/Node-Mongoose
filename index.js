a479b7458180b8fbbe00627fe78e709e892772e1
Alekhya <vinnakotaalekhya@gmail.com>
2020-08-14 19:55:07 -0500

    defining subdocument in the  schema

--- index.js
+++ index.js
@@ -1,40 +1,56 @@
 const mongoose = require('mongoose');
 const Campsite = require('./models/campsite');
 
-const url = "mongodb://localhost:27017/nucampsite/";
+const url = "mongodb://localhost:27017/nucampsite";
 
 //connecting to the db server by passing the conncetion string
-const connect = mongoose.connect(url,{
+const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-connect.then(()=>{
+connect.then(() => {
     console.log("Connected to the server");
 
     //instantiating the Campsite model
-    const newCampsite = new Campsite({
+    //using create method instead of instantiating the model
+    Campsite.create({
         name: 'React Lake Campground',
         description: 'test'
     })
+        .then(campsite => {
+            console.log(campsite);
 
-    newCampsite.save()
-    .then(campsite => {
-        console.log(campsite);
-        return Campsite.find();//using the static method 'find' of the model 
-    })
-    .then(campsite=>{
-        console.log(campsite);
-        return Campsite.deleteMany();//using the static method 'deleteMany' of the model 
-    })
-    .then(()=>{
-        return mongoose.connection.close();//closing the connection
-    })
-    .catch((err)=>{
-        console.log(err);
-        mongoose.connection.close();
-    })
+            return Campsite.findByIdAndUpdate(campsite._id, {
+                $set: { description: "Updated" }
+            },
+                {
+                    new: true
+                });//using the static method 'findByIdAndUpdate' of the model to update a field of the document
+        })
+        .then(campsite => {
+            console.log(campsite);
+            //pushing a subdocument
+            campsite.comments.push({
+                rating: 5,
+                text: 'What a magnificent view!',
+                author: 'Tinus Lorvaldes'
+            })
+            return campsite.save();
+
+        })
+        .then(campsite => {
+            console.log(campsite);
+            return Campsite.deleteMany();//using the static method 'deleteMany' of the model 
+        })
+        .then(() => {
+            return mongoose.connection.close();//closing the connection
+        })
+        .catch((err) => {
+            console.log(err);
+            mongoose.connection.close();
+        })
 
 
 })
--- models/campsite.js
+++ models/campsite.js
@@ -1,22 +1,42 @@
 const mongoose = require('mongoose');
-const Schema=mongoose.Schema;//using the schema function of the mongoose
+const Schema = mongoose.Schema;//using the schema function of the mongoose
+
+const commentSchema = new Schema({
+   rating:{
+       type:Number,
+       min:1,
+       max:5,
+       required:true
+   } ,
+   text:{
+       required:true,
+       type:String
+   },
+   author:{
+    required:true,
+    type:String
+    }},{
+        timestamps:true
+    }
+)
 
 //defining a schema with fields, their data types and any constraints
 const campsiteScheme = new Schema({
-    name:{
-        type:"string",
-        required:true,
-        unique:true
+    name: {
+        type: String,
+        required: true,
+        unique: true
     },
-    description:{
-        type:"string",
-        required:true
-    }
-},{
-    timestamps:true
+    description: {
+        type: String,
+        required: true
+    },
+    comments :[commentSchema]
+}, {
+    timestamps: true
 })
 
 //creating a model that uses this schema for the documents in 'campsites' collection
-const Campsite = new mongoose.model('Campsite',campsiteScheme);
+const Campsite = new mongoose.model('Campsite', campsiteScheme);
 
 module.exports = Campsite;//exporting the model
